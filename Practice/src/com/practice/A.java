51e162bb4c22284459ed2ff43809b84ae62fe492
ganeshnayak17 <99874307+ganeshnayak17@users.noreply.github.com>
2024-03-19 22:50:09 +0530

    Update A.java

--- Practice/src/com/practice/A.java
+++ Practice/src/com/practice/A.java
@@ -4,9 +4,9 @@ public class A {
 
 	public static void main(String[] args) 
 	{
-<<<<<<< HEAD
+
         System.out.println("main");
-=======
+
 
         System.out.println("bivo");
         System.out.println("bo");
@@ -15,7 +15,7 @@ public class A {
         System.out.println("bjbo");
         System.out.println("bo");
 
->>>>>>> a80ee614313f596570aefa6283b7e02e0c234d7a
+
 	}
 
 }
