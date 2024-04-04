94994398520d1fa37dceb23aa8e881112aceaa57
ganeshnayak17 <ganeshnayak7575@gmail.com>
2024-04-04 17:20:52 +0530

    a

--- Practice/src/com/practice/A.java
+++ Practice/src/com/practice/A.java
@@ -1,21 +1,46 @@
 package com.practice;
 
+import java.util.Scanner;
+
 public class A {
 
 	public static void main(String[] args) 
 	{
-
-        System.out.println("main");
-
-
-        System.out.println("bivo");
-        System.out.println("bo");
-
-
-        System.out.println("bjbo");
-        System.out.println("bo");
-
-
-	}
+          Scanner sc = new Scanner(System.in);
+          int i=sc.nextInt();
+          if(i==1)
+          {
+        	  System.out.println("Sunday");
+          }
+          else if(i==2)
+          {
+        	  System.out.println("Monday");
+          }
+          else if(i==3)
+          {
+        	  System.out.println("Tuesday");
+          }
+          else if(i==4)
+          {
+        	  System.out.println("Wednesday");
+          }
+          else if(i==5)
+          {
+        	  System.out.println("Thursday");
+          }
+          else if(i==6)
+          {
+        	  System.out.println("Friday");
+          }
+           else if(i==7)
+          {
+        	  System.out.println("Saturday");
+          }
+          else
+          {
+        	  System.out.println("Invalid Input");
+          }
+          
+    }
 
 }
